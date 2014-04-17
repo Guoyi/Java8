542deae15086cb966c1ad2507ce66541ebf1b281
kzhang <guoyi.zhang@gmail.com>
2014-04-17 16:54:00 +0800

    add new test method

--- src/com/symbio/guoyi/test/JavaTest.java
+++ src/com/symbio/guoyi/test/JavaTest.java
@@ -1,6 +1,11 @@
 package com.symbio.guoyi.test;
 
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import com.symbio.guoyi.domain.Employee;
 
@@ -10,7 +15,12 @@ public class JavaTest {
 		JavaTest test = new JavaTest();
 		
 //		test.testEmployee();
-		test.testRandom();
+		
+//		test.testRandom();
+		
+//		test.testMap();
+		
+		test.testAsynchronous();
 		
 	}
 	
@@ -29,6 +39,56 @@ public class JavaTest {
 			i++;
 		}
 		
+	}
+	
+	public void testMap() {
+		Map<String, String> map = new HashMap<String, String>();
+		
+		Map<String, String> table = new Hashtable<String, String>();
+		
+		map.put(null, "empty");
+		map.put("1", "1");
+		map.put("2", "2");
+		
+		// NullPointerException
+//		table.put(null, "NULL"); 
+		table.put("1", "1");
+		table.put("2", "2");
+		
+		System.out.println(map.get(null));
+	}
+	
+	public void testAsynchronous() {
+		System.out.println("Start main method ... ");
+		
+		ExecutorService exe = Executors.newCachedThreadPool();
+		
+		exe.execute(new Runnable() {
+			
+			@Override
+			public void run() {
+				System.out.println("Start runnable method ... ");
+				
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				System.out.println("End runnable method ... ");
+				
+			}
+		});
+		
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("End main method ...");
 	}
 
 }
